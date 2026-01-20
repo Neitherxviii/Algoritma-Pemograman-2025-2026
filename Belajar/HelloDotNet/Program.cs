cf1d9fb4d631a45d3fb7377fba6e335fd86d2867
FikryRizky <you@example.com>
2026-01-20 14:33:38 +0700

    Done

---
+++ Belajar/HelloDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+  static void Main()
+  {
+    int umur = 18;
+    if (umur < 18)
+    {
+      Console.WriteLine("Anda masih di bawah umur.");
+    }
+    else
+    {
+      Console.WriteLine("Anda sudah dewasa.");
+    }
+  }
+}
